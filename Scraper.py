c08227e675d7ccb358388a6f3a5a6ec3ae2eabb5
Elias <elias.segouat@gmail.com>
2022-01-12 17:04:45 +0100

    Requete qui liste les recherches tendances en france

--- Scraper.py
+++ Scraper.py
@@ -5,5 +5,4 @@ pytrends = TrendReq(hl='en-US', tz=360)
 kw_list = ["Football"]
 pytrends.build_payload(kw_list, cat=0, timeframe='today 5-y', geo='', gprop='')
 
-print(pytrends.related_topics())
-print(pytrends.related_queries())
+print(pytrends.trending_searches(pn='france'))
