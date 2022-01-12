f70d3be7d7bf32a07d7ae3148dff363f05616219
Elias <elias.segouat@gmail.com>
2022-01-12 17:33:37 +0100

    Plusieurs pays (France, USA, Japon, Russie)

--- Scraper.py
+++ Scraper.py
@@ -2,8 +2,11 @@ from pytrends.request import TrendReq
 
 pytrends = TrendReq(hl='en-US', tz=360)
 
-#Creation du payload
+#Payload creation
 #kw_list = ["Football"]
 #pytrends.build_payload(kw_list, cat=0, timeframe='today 5-y', geo='', gprop='')
 
-print(pytrends.trending_searches(pn='france'))
+print(pytrends.trending_searches(pn='france'))# trending searches in real time for France
+print(pytrends.trending_searches(pn='united_states')) 
+print(pytrends.trending_searches(pn='japan'))
+print(pytrends.trending_searches(pn='russia'))
