f355b6f23724f7ed276d0421b5ebc6af5f998e29
Elias <elias.segouat@gmail.com>
2022-01-12 16:55:17 +0100

    Test d'une connexion à google trends et d'une requete basique

--- Scraper.py
+++ Scraper.py
@@ -1,1 +1,8 @@
-print('Hello World')
+from pytrends.request import TrendReq
+
+pytrends = TrendReq(hl='en-US', tz=360)
+
+kw_list = ["Football"]
+pytrends.build_payload(kw_list, cat=0, timeframe='today 5-y', geo='', gprop='')
+
+print(pytrends.related_topics())
